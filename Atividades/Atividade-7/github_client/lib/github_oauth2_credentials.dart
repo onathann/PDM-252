1843164e6ea27a4ed5463bcc90b7a81312d94a58
Nathanael Coelho <72670528+onathann@users.noreply.github.com>
2026-01-09 13:16:12 -0300

    Add files via upload

---
+++ Atividades/Atividade-7/github_client/lib/github_oauth2_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23ligcytlMKN6xGLov';
+const githubClientSecret = '7973867342490a8411e4fd986cb52677fd01f906';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
